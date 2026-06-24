dd1ae9136634fbb6f8b2abc10e279cd02c66f78d
李燃东 <lirandong@maxrocky.com>
2026-06-24 20:33:13 +0800

    fix

--- packages/app/src/utils/file.ts
+++ packages/app/src/utils/file.ts
@@ -1,8 +1,8 @@
 // 下载文件
 export function downloadFile(url: string, fileName: string) {
-  const a = document.createElement('a')
+  const a = document.createElement("a")
   a.href = url
-  a.target = '_blank'
+  a.target = "_blank"
   a.download = fileName
   a.click()
 }
